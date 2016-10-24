8a1b3d8446cb72f4ba37cb89045eccc327543c16
jjzhu <jjzhu_ncu@163.com>
2016-10-24 20:24:12 +0800

    class

---
+++ src/main/java/study/hard/jjzhu/class_/TestClass.java
@@ -0,0 +1,8 @@
+package study.hard.jjzhu.class_;
+
+public class TestClass {
+	private int m;
+	public int inc(){
+		return m + 1;
+	}
+}
---
+++ src/main/java/study/hard/jjzhu/class_/package-info.java
@@ -0,0 +1,8 @@
+/**   
+* @Title: package-info.java 
+* @Package study.hard.jjzhu.class_ 
+* @Description: TODO
+* @author 祝佳俊 jjzhu_ncu@163.com   
+* @date 2016年10月21日 下午5:03:55  
+*/
+package study.hard.jjzhu.class_;
